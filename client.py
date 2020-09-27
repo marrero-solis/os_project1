952f95f01432e42877ef3fbe5cbd82febe45275f
marrero-solis <marrerosolis@gmail.com>
2020-09-27 18:43:11 -0400

    have client send multiple device messages to server.

--- client.py
+++ client.py
@@ -5,11 +5,11 @@ from threading import Thread
 from random import randint
 
 
-# SOCK_DGRAM is the socket type to use for UDP sockets
+# Connect with server through UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Read edevice ID from run command
-deviceID = sys.argv[1]
+#deviceID = sys.argv[1]
 # Initialize <server address> and <server port>
 HOST, PORT = "localhost", 4017
 #data = " ".join(sys.argv[1:])
@@ -21,28 +21,35 @@ class edevice(Thread):
         Thread.__init__(self)
 
     def run(self):
-        # Get random amount of time in the compute server
-        job_time = randint(1,10)
-        # Send message to server
-        message = "{}:{}".format(self.ID,job_time)
-        sock.sendto(bytes(message, "utf-8"), (HOST, PORT))
+        for i in range(max_jobs):
+            # Get random amount of time in the compute server
+            job_time = randint(1,10)
 
-        # Format of the socket object: socket.recv(buffersize[,flags])
-        received = str(sock.recv(1024), "utf-8")
+            # Send message to server
+            message = "{}:{}".format(self.ID,job_time)
+            sock.sendto(bytes(message, "utf-8"), (HOST, PORT))
 
-        print("Sent:     {}".format(message))
-        print("Received: {}".format(received))
-        sleep_time = randint(1,5)
-        time.sleep(sleep_time)
-        print("I'm done, slept for {} seconds".format(sleep_time))
+            # Format of the socket object: socket.recv(buffersize[,flags])
+            #received = str(sock.recv(1024), "utf-8")
+
+            print("Sent: {}".format(message))
+            #print("Received: {}".format(received))
+            sleep_time = randint(1,5)
+            time.sleep(sleep_time)
+            print("Device {} slept for {} seconds".format(self.ID, sleep_time))
 
 def main():
-    # Create edevice Thread object
-    myDevice = edevice(deviceID)
-    # Start running the device thread
-    myDevice.start()
-    # Wait for thread to be done
-    myDevice.join()
+    device_amount = 10
+    myDevices = [0] * device_amount
+
+    # Create N threads to fill the buffer and start the threads
+    for i in range(device_amount):
+        myDevices[i] = edevice(i)
+        myDevices[i].start()
+
+    # Make the original thread wait for the created threads.
+    for i in range(device_amount):
+        myDevices[i].join()
 
 if __name__ == "__main__":
     main()
